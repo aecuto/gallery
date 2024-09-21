d048f0d85d36270e247ec1b636e5891f311f332b
aecuto <kampee.expertly01@gmail.com>
2024-09-21 14:54:46 +0700

    add gallery page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
+import { generate } from "random-words";
+import InfiniteScroll from "react-infinite-scroll-component";
+interface Photo {
+  id: string;
+  download_url: string;
+  hashtags: string[];
+}
+
+const words = generate(Number(process.env.NEXT_PUBLIC_HASHTAGS) || 5);
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [hashtag, setHashTag] = useState<string>("");
+  const [page, setPage] = useState(1);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const fetchData = () => {
+    axios
+      .get(`https://picsum.photos/v2/list?page=${page}&limit=15`)
+      .then((res: any) => {
+        const data = res.data.map((data: any[]) => {
+          const random = Math.floor(Math.random() * words.length);
+          return {
+            ...data,
+            hashtags: words.slice(random),
+          };
+        });
+        setPhotos((prev) => [...prev, ...data]);
+      });
+    setPage((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const data = useMemo(() => {
+    return photos.filter((photo) => {
+      return !hashtag ? true : photo.hashtags.includes(hashtag);
+    });
+  }, [hashtag, photos]);
+
+  return (
+    <div className="p-10">
+      <InfiniteScroll
+        dataLength={photos.length}
+        next={fetchData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {hashtag && (
+          <div
+            className="bg-blue-500 hover:bg-blue-800 p-1 rounded w-fit text-center mb-3 cursor-pointer"
+            onClick={() => setHashTag("")}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Clear (#{hashtag})
+          </div>
+        )}
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+          {data?.map((photo) => (
+            <div key={photo.id}>
+              <img
+                className="object-cover object-center w-full h-40 max-w-full rounded-lg"
+                alt="gallery-photo"
+                src={photo.download_url}
+              />
+              <div className="flex flex-wrap">
+                {photo.hashtags.map((tag) => (
+                  <div
+                    key={tag}
+                    className="mr-1 hover:text-blue-500 cursor-pointer"
+                    onClick={() => setHashTag(tag)}
+                  >
+                    #{tag}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </InfiniteScroll>
     </div>
   );
 }
